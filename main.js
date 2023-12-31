ae286cca5e610dfd2bb7d9d312bd5d1344b62215
22KShah-hub-art <120753197+22KShah-hub-art@users.noreply.github.com>
2023-12-31 16:52:07 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+
+
+function setup() {
+  // Create the canvas
+  createCanvas(475, 400);
+  capture = createCapture(VIDEO);
+  capture.size(320, 320);
+  //capture.hide();
+
+  // Set colors
+  fill(204, 101, 192, 127);
+  stroke(127, 63, 120);
+
+  // A rectangle
+  rect(40, 120, 120, 40);
+  // An ellipse
+  ellipse(240, 240, 80, 80);
+  // A triangle
+  triangle(300, 100, 320, 100, 310, 80);
+
+  // A design for a simple flower
+  translate(580, 200);
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 30, 20, 80);
+    rotate(PI/5);
+  }
+}
